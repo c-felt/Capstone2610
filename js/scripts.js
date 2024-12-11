11a6be4e63bc523798b9f25ccaed9faa35d821b2
Claire Felt <cfeltgungatan@gmail.com>
2024-12-11 00:54:32 -0700

    Tons of additions and edits to color font and footer

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@ btnElement.addEventListener('click', () => {
     btnElement.classList.toggle('open');
     navElement.classList.toggle('open');
 }); 
+
+//Put current year in footer
+const theYear = new Date();
+document.querySelector('#year').textContent = theYear.getFullYear()
